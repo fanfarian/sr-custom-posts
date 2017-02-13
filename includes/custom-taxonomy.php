e806c9f8f442ac043c629dcc6048f991d2f62cd6
Stefan Reichert <reichert@qu-int.com>
2017-02-13 09:30:20 +0100

    Small Taxonomy Update

--- includes/custom-taxonomy.php
+++ includes/custom-taxonomy.php
@@ -42,64 +42,24 @@ class srcp_custom_posts_Taxonomies {
 	    
 	    $args = array(
 	   		'label' 				=> __( 'TAX 1', 'srcp'  ),
-			'labels' 				=> $labels,
-			'public'       			=> false,
-			'show_ui'				=> true,
-			'show_in_nav_menus' 	=> false,
-			'show_tagcloud'			=> false,
-			'meta_box_cb'			=> false,
-			'show_admin_column' 	=> true,
-			'hierarchical' 			=> false,
+			'labels'				=> $labels,
+			'hierarchical'          => false,
+			'public'                => true,
+			'show_ui'               => true,
+			'show_admin_column'     => true,
+			'show_in_nav_menus'     => true,
+			'show_tagcloud'         => false,
 			'query_var' 			=> true,
+			'meta_box_cb'			=> true,
+			'show_in_quick_edit'	=> true,
 	        'rewrite'       		=> false,	// array( 'slug' => 'TAX_1_SLUG', 'with_front' => false),
+			'show_in_rest'			=> true,
+	        'rest_base'				=> 'surfaces-api',
 	    );
-	    register_taxonomy( 'POST_1_TAX_1', 'POST_1', $args );
+	    register_taxonomy( 'POST_1_TAX_1', array( 'POST_1' ), $args );
+	    
 	}
 	
-	/**
-     * Register Taxonomy 2
-     *
-     * @since  1.0.0
-     * @access public
-     * @return void
-     */
-    public function register_taxonomy_TAX_2() {	
-		$labels = array(
-			'name'                       => _x( 'TAX 2', 'taxonomy general name', 'srcp' ),
-			'singular_name'              => _x( 'TAX 2', 'taxonomy singular name', 'srcp' ),
-			'menu_name'                  => __( 'TAX 2', 'srcp' ),			    
-
-			'search_items'               => __( 'Search TAX 2', 'srcp' ),
-			'popular_items'              => __( 'Popular TAX 2', 'srcp' ),
-			'all_items'                  => __( 'All TAX 2', 'srcp' ),
-			'view_item'					 => __( 'Show TAX 2', 'srcp' ),
-			'parent_item'                => __( 'TAX 2 Parent Item', 'srcp' ),
-			'parent_item_colon'          => __( 'TAX 2 Parent Item:', 'srcp' ),
-			'edit_item'                  => __( 'Edit TAX 2', 'srcp' ),
-			'update_item'                => __( 'Update TAX 2', 'srcp' ),
-			'add_new_item'               => __( 'Add New TAX 2', 'srcp' ),
-			'new_item_name'              => __( 'New TAX 2 Name', 'srcp'  ),
-			'separate_items_with_commas' => __( 'Separate TAX 2 with commas', 'srcp'  ),
-			'add_or_remove_items'        => __( 'Add or remove TAX 2', 'srcp'  ),
-			'choose_from_most_used'      => __( 'Choose from the most used TAX 2', 'srcp'  ),
-			'not_found'                  => __( 'No TAX 2 found.', 'srcp'  ),
-	    );
-	    
-	    $args = array(
-	   		'label' 				=> __( 'TAX 2', 'srcp'  ),
-			'labels' 				=> $labels,
-			'public'       			=> false,
-			'show_ui'				=> true,
-			'show_in_nav_menus' 	=> false,
-			'show_tagcloud'			=> false,
-			'meta_box_cb'			=> false,
-			'show_admin_column' 	=> true,
-			'hierarchical' 			=> false,
-			'query_var' 			=> true,
-	        'rewrite'       		=> false,	// array( 'slug' => 'TAX_2_SLUG', 'with_front' => false),
-	    );
-	    register_taxonomy( 'POST_1_TAX_2', 'POST_1', $args );
-	}
 	
 	/**
      * Register more taxonomies
